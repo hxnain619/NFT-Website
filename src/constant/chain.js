4e5d08aaa4e474dc9064fa72033e0d5c0723da8f
Wiktor Nowak <w.nowak.work@gmail.com>
2023-10-08 13:54:15 -0600

    [BUG] ethereum connect issue

--- src/constant/chain.js
+++ src/constant/chain.js
@@ -7,8 +7,9 @@ const EVM_CHAINS = {
 const CHAIN_DECIMAL_TO_HEX = {
   137: "0x89",
   80001: "0x13881",
-  43113: "0Xa869",
+  43113: "0xa869",
   43114: "0xa86a",
+  5: "0x5",
 };
 
 export { EVM_CHAINS, CHAIN_DECIMAL_TO_HEX };
